470a808c0f1b2178761523783e0a62d54fda0f65
Tom Xu <tomxu89@gmail.com>
2018-01-20 00:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ function checkCollision(rock) {
 
     if (
       (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
-      (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) || 
-      (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)) {
+      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) || 
+      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
       return true
     }
   }
