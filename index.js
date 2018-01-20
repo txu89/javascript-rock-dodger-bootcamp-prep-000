c22e57cb480fa2c8c7749b4895d36ab56adae2e9
Tom Xu <tomxu89@gmail.com>
2018-01-20 00:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function endGame() {
   // for (let i = 0; i < ROCKS.length; i++) {
   //   GAME.removeChild(ROCKS[i])
   // }
-  // window.removeEventListener('keydown', moveDodger)
+  window.removeEventListener('keydown', moveDodger)
   return alert('YOU LOSE!')
 }
 
