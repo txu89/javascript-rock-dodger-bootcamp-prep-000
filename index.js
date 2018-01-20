f7ea528d49e820c8ed4bf5498d0bfa07095fd0c3
Tom Xu <tomxu89@gmail.com>
2018-01-20 01:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,8 +94,11 @@ function endGame() {
 function moveDodger(e) {
   if(e.which === LEFT_ARROW) {
     moveDodgerLeft()
+    e.preventDefault()
+    e.stopPropagation()
   } else if (e.which === RIGHT_ARROW) {
     moveDodgerRight()
+    e.preventDefault()
   }
 }
 
